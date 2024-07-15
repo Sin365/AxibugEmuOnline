ac3f78f8df529c9ad52905d255e94ac8b128d069
sin365 <353374337@qq.com>
2024-07-15 16:12:09 +0800

    新增WebApi

---
+++ AxibugEmuOnline.Web/Program.cs
@@ -0,0 +1,37 @@
+using AxibugEmuOnline.Web.Common;
+
+namespace AxibugEmuOnline.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Config.LoadConfig();
+            Haoyue_SQLPoolManager.InitConnMgr();
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
