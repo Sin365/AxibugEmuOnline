2884bc2a9e91e5b4dfdaca80f9486762dc700708
sin365 <353374337@qq.com>
2024-08-04 00:32:14 +0800

    transfer Mapper114~164

---
+++ AxibugEmuOnline.Client/Assets/VirtualNes.Core/Mapper/Mapper133.cs
@@ -0,0 +1,38 @@
+﻿//////////////////////////////////////////////////////////////////////////
+// Mapper133  SACHEN CHEN                                               //
+//////////////////////////////////////////////////////////////////////////
+using static VirtualNes.MMU;
+using static VirtualNes.Core.CPU;
+using INT = System.Int32;
+using BYTE = System.Byte;
+using System;
+using Codice.CM.Client.Differences;
+
+namespace VirtualNes.Core
+{
+    public class Mapper133 : Mapper
+    {
+        public Mapper133(NES parent) : base(parent)
+        {
+        }
+
+        public override void Reset()
+        {
+            SetPROM_32K_Bank(0);
+            SetVROM_8K_Bank(0);
+        }
+
+        //void Mapper133::WriteLow(WORD addr, BYTE data)
+        public override void WriteLow(ushort addr, byte data)
+        {
+            if (addr == 0x4120)
+            {
+                SetPROM_32K_Bank((data & 0x04) >> 2);
+                SetVROM_8K_Bank(data & 0x03);
+            }
+            CPU_MEM_BANK[addr >> 13][addr & 0x1FFF] = data;
+        }
+
+
+    }
+}
