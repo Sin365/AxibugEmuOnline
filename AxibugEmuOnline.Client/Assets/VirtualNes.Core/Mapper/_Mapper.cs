a5ec002da9d81a7db6ed2f96a71c3cbd48e58160
sin365 <353374337@qq.com>
2024-08-05 00:44:30 +0800

    transfer Mapper 43-68

--- AxibugEmuOnline.Client/Assets/VirtualNes.Core/Mapper/_Mapper.cs
+++ AxibugEmuOnline.Client/Assets/VirtualNes.Core/Mapper/_Mapper.cs
@@ -2,17 +2,17 @@
 using static VirtualNes.Core.CPU;
 using INT = System.Int32;
 using BYTE = System.Byte;
-using System;
 
 namespace VirtualNes.Core
 {
-    public class _MapName : Mapper
-    {
-		public _MapName(NES parent) : base(parent)
-        {
-        }
+	public class _Mapper : Mapper
+	{
+		public _Mapper(NES parent) : base(parent)
+		{
+		}
 
-        public override void Reset()
+
+		public override void Reset()
 		{
 		}
 
