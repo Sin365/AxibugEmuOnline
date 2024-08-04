b91e3a0e65e9ec84437fa5585b1abfed81043253
sin365 <353374337@qq.com>
2024-08-05 01:33:07 +0800

    transfer Mapper 18-43

---
+++ AxibugEmuOnline.Client/Assets/VirtualNes.Core/Mapper/Mapper018.cs
@@ -0,0 +1,23 @@
+﻿//////////////////////////////////////////////////////////////////////////
+// Mapper018  Jaleco SS8806                                             //
+//////////////////////////////////////////////////////////////////////////
+using static VirtualNes.MMU;
+using static VirtualNes.Core.CPU;
+using INT = System.Int32;
+using BYTE = System.Byte;
+
+namespace VirtualNes.Core
+{
+	public class Mapper018 : Mapper
+	{
+		public Mapper018(NES parent) : base(parent)
+		{
+		}
+
+
+		public override void Reset()
+		{
+		}
+
+	}
+}
