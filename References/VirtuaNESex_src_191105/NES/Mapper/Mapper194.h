aa08afa3c7dc68cf1adfcfcc4570c59bd13ae3e0
sin365 <353374337@qq.com>
2024-08-05 17:58:53 +0800

    readme + references

---
+++ References/VirtuaNESex_src_191105/NES/Mapper/Mapper194.h
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////////////////////////////
+// Mapper194  WaiXingTypeD Base ON Nintendo MMC3                        //
+//////////////////////////////////////////////////////////////////////////
+class	Mapper194 : public Mapper
+{
+public:
+	Mapper194( NES* parent ) : Mapper(parent) {}
+
+	
+	void	Reset();
+	void	Write( WORD addr, BYTE data );
+	void	WriteLow( WORD addr, BYTE data );
+	BYTE	ReadLow( WORD addr );
+
+	void	HSync( INT scanline );
+
+	// For state save
+	BOOL	IsStateSave() { return TRUE; }
+	void	SaveState( LPBYTE p );
+	void	LoadState( LPBYTE p );
+
+protected:
+	BYTE	reg[8];
+	BYTE	prg[4];
+	BYTE	chr[8];
+
+	BYTE	irq_type;
+	BYTE	irq_enable;
+	BYTE	irq_counter;
+	BYTE	irq_latch;
+	BYTE	irq_request;
+
+private:
+	void	SetBank_CPU();
+	void	SetBank_PPU();
+};
+
