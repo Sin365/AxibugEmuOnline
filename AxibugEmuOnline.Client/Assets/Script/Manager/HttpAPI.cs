71b4d857d4af7bda764ed5a994508ee40e695f1b
ALIENJACK\alien <alienjack@foxmail.com>
2024-08-13 18:35:20 +0800

    Rom下载系统

---
+++ AxibugEmuOnline.Client/Assets/Script/Manager/HttpAPI.cs
@@ -0,0 +1,51 @@
+﻿using AxibugEmuOnline.Client.ClientCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace AxibugEmuOnline.Client
+{
+    public class HttpAPI
+    {
+        public string WebSite = "http://emu.axibug.com/api";
+
+        public void GetNesRomList(Action<Resp_GameList> callback, int page, int pageSize = 10)
+        {
+            AppAxibugEmuOnline.StartCoroutine(GetNesRomListFlow(page, pageSize, callback));
+        }
+
+        private IEnumerator GetNesRomListFlow(int page, int pageSize, Action<Resp_GameList> callback)
+        {
+            UnityWebRequest request = new UnityWebRequest($"{WebSite}/NesRomList?Page={page}&PageSize={pageSize}");
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                callback.Invoke(null);
+                yield break;
+            }
+
+            var resp = JsonUtility.FromJson<Resp_GameList>(request.downloadHandler.text);
+            callback.Invoke(resp);
+        }
+
+        public class Resp_GameList
+        {
+            public int Page { get; set; }
+            public int MaxPage { get; set; }
+            public int ResultAllCount { get; set; }
+            public List<Resp_RomInfo> GameList { get; set; }
+        }
+
+        public class Resp_RomInfo
+        {
+            public int ID { get; set; }
+            public string Hash { get; set; }
+            public string RomName { get; set; }
+            public string Url { get; set; }
+            public string ImgUrl { get; set; }
+        }
+    }
+}
---
+++ AxibugEmuOnline.Client/Assets/Script/Manager/RomLib/RomFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace AxibugEmuOnline.Client
+{
+    public class RomFile
+    {
+        private HttpAPI.Resp_RomInfo webData;
+        private bool hasLocalFile;
+        private string romName;
+        private EnumPlatform platform;
+
+        public string LocalFilePath => $"{Application.persistentDataPath}/RemoteRoms/{platform}/{romName}";
+
+
+        public RomFile(EnumPlatform platform)
+        {
+            this.platform = platform;
+        }
+
+        public void GetRomFileData(Action<byte[]> callback)
+        {
+            if (webData == null) { callback.Invoke(null); return; }
+            if (hasLocalFile)
+            {
+                var path = LocalFilePath;
+            }
+        }
+
+        public void SetWebData(HttpAPI.Resp_RomInfo resp_RomInfo)
+        {
+            webData = resp_RomInfo;
+            romName = Path.GetFileName(webData.Url);
+
+            if (File.Exists(LocalFilePath))
+            {
+                hasLocalFile = true;
+            }
+            else
+            {
+                hasLocalFile = false;
+            }
+        }
+    }
+}
