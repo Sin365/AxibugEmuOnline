c8f5dfddda60b90ae2aa460c1124db45a626b1b8
ALIENJACK\alien <alienjack@foxmail.com>
2024-09-11 16:48:23 +0800

    增加了移除Cache和Rom文件的api

--- AxibugEmuOnline.Client/Assets/Script/UI/LaunchUI.cs
+++ AxibugEmuOnline.Client/Assets/Script/UI/LaunchUI.cs
@@ -2,8 +2,6 @@ using AxibugEmuOnline.Client.UI;
 using DG.Tweening;
 using DG.Tweening.Core;
 using DG.Tweening.Plugins.Options;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace AxibugEmuOnline.Client
