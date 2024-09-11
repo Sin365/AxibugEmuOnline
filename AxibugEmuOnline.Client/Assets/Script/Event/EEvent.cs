bd45825bf5ff0b0d2d967179bbb0a39cb82bf4d7
sin365 <353374337@qq.com>
2024-09-11 18:10:47 +0800

    完成基础房间逻辑封装 | NetRePlay

--- AxibugEmuOnline.Client/Assets/Script/Event/EEvent.cs
+++ AxibugEmuOnline.Client/Assets/Script/Event/EEvent.cs
@@ -3,6 +3,39 @@
     public enum EEvent
     {
         // 添加你自己需要的事件类型
-        OnChatMsg
+        OnChatMsg,
+
+
+        OnRoomListAllUpdate,//房间列表全量刷新
+        OnRoomListSingleUpdate,//房间列表中单个更新
+
+        /// <summary>
+        /// 我进入房间
+        /// </summary>
+        OnMineJoinRoom,
+        /// <summary>
+        /// 我离开房间
+        /// </summary>
+        OnMineLeavnRoom,
+
+        /// <summary>
+        /// 其他人进入房间
+        /// </summary>
+        OnOtherPlayerJoinRoom,
+
+        /// <summary>
+        /// 其他人离开房间
+        /// </summary>
+        OnOtherPlayerLeavnRoom,
+
+        /// <summary>
+        /// 服务器等待Step更新
+        /// </summary>
+        OnRoomWaitStepChange,
+
+        /// <summary>
+        /// 要求加载即时存档
+        /// </summary>
+        OnRoomNeedLoadRawData,
     }
 }
