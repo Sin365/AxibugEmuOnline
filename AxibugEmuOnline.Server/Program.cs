d567cd9e7123de5343fa1174468f15f59fe35ee3
sin365 <353374337@qq.com>
2024-09-13 10:10:14 +0800

    服务端逻辑修改

--- AxibugEmuOnline.Server/Program.cs
+++ AxibugEmuOnline.Server/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AxibugEmuOnline.Server
+﻿using AxibugEmuOnline.Server.Manager;
+
+namespace AxibugEmuOnline.Server
 {
     internal class Program
     {
@@ -8,9 +10,28 @@
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Title = Title;
             AppSrv.InitServer(10492);
-            while (true) 
+            while (true)
             {
-                Console.ReadLine();
+                string CommandStr = Console.ReadLine();
+                string Command = "";
+                Command = ((CommandStr.IndexOf(" ") <= 0) ? CommandStr : CommandStr.Substring(0, CommandStr.IndexOf(" ")));
+                switch (Command)
+                {
+                    case "list":
+                        {
+                            AppSrv.g_Log.Info("当前在线:" + AppSrv.g_ClientMgr.GetOnlineClient());
+                            var onlinelist = AppSrv.g_ClientMgr.GetOnlineClientList();
+                            for (int i = 0; i < onlinelist.Count; i++)
+                            {
+                                ClientInfo cinfo = onlinelist[i];
+                                AppSrv.g_Log.Info($"UID->{cinfo.UID}   Name->{cinfo.NickName}  Ping->{cinfo.AveNetDelay}");
+                            }
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("未知命令" + CommandStr);
+                        break;
+                }
             }
         }
     }
