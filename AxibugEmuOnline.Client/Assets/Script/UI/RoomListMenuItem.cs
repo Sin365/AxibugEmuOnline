9c168a5e29afce5d503a5e8b978fe4851cc659ce
ALIENJACK\alien <alienjack@foxmail.com>
2024-11-07 19:18:50 +0800

    房间列表获取,调用实装

--- AxibugEmuOnline.Client/Assets/Script/UI/RoomListMenuItem.cs
+++ AxibugEmuOnline.Client/Assets/Script/UI/RoomListMenuItem.cs
@@ -1,34 +1,45 @@
 using AxibugEmuOnline.Client.ClientCore;
-using AxibugProtobuf;
+using AxibugEmuOnline.Client.Event;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace AxibugEmuOnline.Client
 {
     public class RoomListMenuItem : VirtualSubMenuItem
     {
+        bool m_entering;
+
+        protected override void Awake()
+        {
+            Eventer.Instance.RegisterEvent<int>(EEvent.OnRoomListAllUpdate, OnRoomListUpdateAll);
+        }
+
+        public override bool OnEnterItem()
+        {
+            var res = base.OnEnterItem();
+            if (res) m_entering = true;
+            return res;
+        }
+
+        public override bool OnExitItem()
+        {
+            var res = base.OnExitItem();
+            if (res) m_entering = false;
+            return res;
+        }
+
+        private void OnRoomListUpdateAll(int obj)
+        {
+            if (m_entering)
+            {
+                RefreshUI();
+            }
+        }
+
         protected override void GetVirtualListDatas(Action<object> datas)
         {
-            App.StartCoroutine(Test(datas));
+            var roomList = App.roomMgr.GetRoomList();
+            datas.Invoke(roomList);
         }
 
-        private IEnumerator Test(Action<object> datas)
-        {
-            yield return null;
-
-            List<Protobuf_Room_MiniInfo> fakeData = new List<Protobuf_Room_MiniInfo>()
-            {
-                new Protobuf_Room_MiniInfo{ GameRomID = 1, RoomID = 1, HostPlayerUID = 1, Player1UID = 1, Player1NickName = "Test1"},
-                new Protobuf_Room_MiniInfo{ GameRomID = 2, RoomID = 2, HostPlayerUID = 2, Player1UID = 2, Player1NickName = "Test2"},
-                new Protobuf_Room_MiniInfo{ GameRomID = 3, RoomID = 3, HostPlayerUID = 3, Player1UID = 3, Player1NickName = "Test3"},
-                new Protobuf_Room_MiniInfo{ GameRomID = 4, RoomID = 4, HostPlayerUID = 4, Player1UID = 4, Player1NickName = "Test4"},
-                new Protobuf_Room_MiniInfo{ GameRomID = 5, RoomID = 5, HostPlayerUID = 5, Player1UID = 5, Player1NickName = "Test5"},
-            };
-
-            datas.Invoke(fakeData);
-
-            yield break;
-        }
     }
 }
