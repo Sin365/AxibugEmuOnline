36293af2b180eaeaf89914f37e097ae37633786c
sin365 <353374337@qq.com>
2024-11-28 19:56:31 +0800

    数据库结构

--- etc/haoyue_emu.sql
+++ etc/haoyue_emu.sql
@@ -11,11 +11,24 @@
  Target Server Version : 100311
  File Encoding         : 65001
 
- Date: 16/07/2024 10:56:10
+ Date: 28/11/2024 19:55:49
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for rom_stars
+-- ----------------------------
+DROP TABLE IF EXISTS `rom_stars`;
+CREATE TABLE `rom_stars`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `platform` int(11) NOT NULL,
+  `romid` int(11) NOT NULL,
+  `logdate` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Fixed;
 
 -- ----------------------------
 -- Table structure for romlist_nes
@@ -29,7 +42,58 @@ CREATE TABLE `romlist_nes`  (
   `GameType` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `Note` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `Hash` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `playcount` int(11) NOT NULL DEFAULT 0,
+  `stars` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`Id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 2702 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for room_log
+-- ----------------------------
+DROP TABLE IF EXISTS `room_log`;
+CREATE TABLE `room_log`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `platform` int(11) NOT NULL,
+  `romid` int(11) NOT NULL,
+  `roomid` int(11) NULL DEFAULT NULL,
+  `state` int(11) NOT NULL,
+  `logdate` datetime NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Fixed;
+
+-- ----------------------------
+-- Table structure for room_log_state
+-- ----------------------------
+DROP TABLE IF EXISTS `room_log_state`;
+CREATE TABLE `room_log_state`  (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user_devices
+-- ----------------------------
+DROP TABLE IF EXISTS `user_devices`;
+CREATE TABLE `user_devices`  (
+  `device` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `devicetype` int(11) NOT NULL,
+  `uid` int(11) NOT NULL
+) ENGINE = MyISAM CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `account` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `regdate` datetime NOT NULL DEFAULT current_timestamp(),
+  `nikename` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `lastlogindate` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`uid`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
