f8cbdc1df7874411fbdaece66c623c13affced1b
ALIENJACK <alienjack@foxmail.com>
2024-12-11 21:29:19 +0800

    目录结构调整撞上了pull...

--- AxibugEmuOnline.Client/Assets/Script/AppMain/Initer.cs
+++ AxibugEmuOnline.Client/Assets/Script/AppMain/Initer.cs
@@ -11,9 +11,18 @@ namespace AxibugEmuOnline.Client
         public CanvasGroup m_xmbBg;
         public static string dev_UUID;
 
+#if UNITY_EDITOR
+        public bool bTest = false;
+        public string mTestSrvIP = "192.168.0.47";
+#endif
+
         private void Awake()
         {
+#if UNITY_EDITOR
+            App.Init(this, bTest, mTestSrvIP);
+#else
             App.Init(this);
+#endif
             dev_UUID = SystemInfo.deviceUniqueIdentifier;
         }
     }
