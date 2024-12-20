495ad8de23255af86147190c63e7a082870c5ffd
ALIENJACK\alien <alienjack@foxmail.com>
2024-12-20 11:06:08 +0800

    XMBTopGroup重命名为XMBInfoBar,不再局限于右上角UI管理 GamingChanger.cs和NormalChanger.cs不再属于legacy了,因为InputSystem已经作废了:)

--- AxibugEmuOnline.Client/Assets/Script/AppMain/UI/XMBTopGroupUI/XMBInfoBar.cs
+++ AxibugEmuOnline.Client/Assets/Script/AppMain/UI/XMBTopGroupUI/XMBInfoBar.cs
@@ -4,7 +4,7 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class XMBTopGroup : MonoBehaviour
+public class XMBInfoBar : MonoBehaviour
 {
     public Text txtDateTime;
     public Image imgPowerRoot;
