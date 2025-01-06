cd49086ddf7dd1d0d8e4ed6ec5d1c34d30220c1f
ALIENJACK\alien <alienjack@foxmail.com>
2025-01-06 16:45:54 +0800

    修复Mapper023错误的reg[9]越界问题(virtuanes源码就有这个问题,但是c++可能不会有数组越界报错问题)

--- AxibugEmuOnline.Client/Assets/VirtualNes.Core/Mapper/Mapper023.cs
+++ AxibugEmuOnline.Client/Assets/VirtualNes.Core/Mapper/Mapper023.cs
@@ -40,7 +40,7 @@ namespace VirtualNes.Core
             irq_latch = 0;
             irq_clock = 0;
 
-            reg[9] = 1;
+            //reg[9] = 1;
 
             SetPROM_32K_Bank(0, 1, PROM_8K_SIZE - 2, PROM_8K_SIZE - 1);
             SetVROM_8K_Bank(0);
