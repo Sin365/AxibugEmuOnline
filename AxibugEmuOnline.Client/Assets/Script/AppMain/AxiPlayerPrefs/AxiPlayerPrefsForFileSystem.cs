7b40e190d012326fd0720e37bab2421503e854ab
sin365 <353374337@qq.com>
2025-06-16 10:56:21 +0800

    引入AxiPlayerPrefs，替换现有PlayerPrefs，使用文件系统，替代不同平台的PlayerPrefs读写

---
+++ AxibugEmuOnline.Client/Assets/Script/AppMain/AxiPlayerPrefs/AxiPlayerPrefsForFileSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AxiPlayerPrefsForFileSystem : AxiPlayerPrefsFileBase
+{
+    public AxiPlayerPrefsForFileSystem() : base(LoadData, SaveData)
+    {
+        Debug.Log($"AxiPlayerPrefsForPSVita Init");
+    }
+
+    public static Dictionary<string, AxiPlayerPrefsKeyValye> LoadData()
+    {
+        if (!AxiIO.AxiIO.io.file_Exists(AxiPlayerPrefsFilePath))
+            return new Dictionary<string, AxiPlayerPrefsKeyValye>();
+        else
+        {
+            string outputData = string.Empty;
+            byte[] loadedData = AxiIO.AxiIO.io.file_ReadAllBytes(AxiPlayerPrefsFilePath);
+            if (loadedData != null && loadedData.Length != 0)
+            {
+                using (System.IO.MemoryStream stream = new System.IO.MemoryStream(loadedData))
+                {
+                    using (System.IO.BinaryReader reader = new System.IO.BinaryReader(stream))
+                    {
+                        outputData = reader.ReadString();
+                    }
+                }
+            }
+            if (string.IsNullOrEmpty(outputData))
+                return new Dictionary<string, AxiPlayerPrefsKeyValye>();
+            return AxiPlayerPrefsFileBase.JsonStrToData(outputData);
+        }
+    }
+
+    public static void SaveData(Dictionary<string, AxiPlayerPrefsKeyValye> data)
+    {
+        string jsonStr = AxiPlayerPrefsFileBase.DataToJsonStr(data);
+        byte[] dataByteArray;
+        using (System.IO.MemoryStream stream = new System.IO.MemoryStream(jsonStr.Length * sizeof(char)))
+        {
+            System.IO.BinaryWriter binaryWriter = new System.IO.BinaryWriter(stream);
+            binaryWriter.Write(jsonStr);
+            dataByteArray = stream.GetBuffer();
+            stream.Close();
+        }
+        AxiIO.AxiIO.io.file_WriteAllBytes(AxiPlayerPrefsFilePath, dataByteArray, false);
+    }
+
+}
---
+++ AxibugEmuOnline.Client/Assets/Script/AppMain/AxiPlayerPrefs/IAxiPlayerPrefs.cs
@@ -0,0 +1,13 @@
+﻿public interface IAxiPlayerPrefs
+{
+    void DeleteAll();
+    float GetFloat(string key, float defaultValue);
+    float GetFloat(string key);
+    int GetInt(string key, int defaultValue);
+    int GetInt(string key);
+    string GetString(string key);
+    string GetString(string key, string defaultValue);
+    void SetFloat(string key, float value);
+    void SetInt(string key, int value);
+    void SetString(string key, string value);
+}
