9bed2f1597776fdffae8097102e901a88cebfda3
sin365 <353374337@qq.com>
2025-10-24 02:03:08 +0800

    PC平台打包目录多一层

--- AxibugEmuOnline.Client/Assets/AxiProjectTools/Editors/AxiAutoBuild.cs
+++ AxibugEmuOnline.Client/Assets/AxiProjectTools/Editors/AxiAutoBuild.cs
@@ -88,8 +88,17 @@ public static class AxiAutoBuild
             _ => "",
         };
 
+        string targetplatformDir = target.ToString();
 
-        string _locationPathName = $"Output/{target}/{targetName}";
+        
+        targetplatformDir += target switch
+        {
+            //PC 平台目录追加一层时间
+            BuildTarget.StandaloneWindows => "/"+ DateTime.Now.ToString("yyyyMMddHHmmss"),
+            _ => "",
+        };
+
+        string _locationPathName = $"Output/{targetplatformDir}/{targetName}";
         string FullPath = Path.GetFullPath(Path.Combine(Application.dataPath, "..", _locationPathName));
         string dirPath = Path.GetDirectoryName(FullPath);
         if (!Directory.Exists(dirPath))
